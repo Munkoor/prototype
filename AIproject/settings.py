e5f58122a9f89a3a9dbee6607d99848a00092f84
Munkoor <eee1lvlboss@gmail.com>
2024-06-19 11:22:36 +0300

    Added PORT

--- AIproject/settings.py
+++ AIproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'prototype-65oy.onrender.com',
 ]
-
+PORT = os.environ.get('PORT', 8000)
 # Application definition
 
 INSTALLED_APPS = [
